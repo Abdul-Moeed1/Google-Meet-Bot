7899d073de879b15d340cb3ffe9878885a420d6c
Abdul Moeed <abdulmoeedjhelumi@gmail.com>
2025-04-06 13:37:35 +0500

    Update Home.jsx

--- client/src/components/UI/Home.jsx
+++ client/src/components/UI/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () =>{
             <div className="max-w-3xl">
               <h1 className="text-4xl font-bold text-blue-600">Welcome to MeetVoice</h1>
               <p className="mt-4 text-lg text-gray-700">
-                MeetVoice is an automated bot that joins Google Meet meetings and plays pre-recorded audio seamlessly.
+                MeetVoice is an automated bot that joins Google Meet meetings and give you transcription of the meeting.
               </p>
               <div className="mt-6">
                 <NavLink to="/start" className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-700 transition">
